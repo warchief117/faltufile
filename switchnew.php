481968d10579aadbcd66ebd5c31146534d62ed20
warchief117 <103111422+warchief117@users.noreply.github.com>
2022-04-06 15:57:12 +0530

    Add files via upload

---
+++ switchnew.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+    <center>
+        <head>
+            <title>
+                portfolio
+            </title>
+        </head>
+        <style>
+            body {
+                background-image : url("code.gif");
+                background-color: grey;            }
+                </style>
+                </head>
+                <body>
+                <h1>atul k</h1>
+                <h2>it consultant</h2>
+                <p>
+                    <a herf ="https://uhdpixel.com/wall/kratos-god-of-war-4-ps4-4k-z115/"></a>
+                </p>
+                <center>
+                    <img scr ="profile.jpg" alt="IT_consultant" width="125" height="125"
+                </center>
+                </body>
+    </center>
